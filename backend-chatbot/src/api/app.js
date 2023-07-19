1f68a588ee2268557fda1da05246e3fb61d0f625
Sheifer Kepler <sheifer.kda@gmail.com>
2023-07-19 13:10:56 -0300

    refactor: add cors in app.js

--- backend-chatbot/src/api/app.js
+++ backend-chatbot/src/api/app.js
@@ -1,7 +1,9 @@
+const cors = require('cors')
 const express = require('express');
 const userRoute = require('../routes/user.routes');
 
 const app = express();
+app.use(cors())
 app.use(express.json());
 
 app.use('/user', userRoute);
